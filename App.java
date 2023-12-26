96656ea1bb5dd9c60655aefe8575591e0d8e21bb
Peter Mora IV <129004174+pdmIV@users.noreply.github.com>
2023-12-25 22:18:14 -0600

    Create App.java

---
+++ App.java
@@ -0,0 +1,25 @@
+package indy;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.FileNotFoundException;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws FileNotFoundException {
+        stage.setScene(new Scene(
+                (new PaneOrganizer()).getRoot(), Constants.BOARD_WIDTH + Constants.CONTROLS_PANE_WIDTH,
+                Constants.BOARD_HEIGHT
+        ));
+        stage.setResizable(false);
+        stage.setTitle("Connect Four");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args); // launch is a method inherited from Application
+    }
+}
